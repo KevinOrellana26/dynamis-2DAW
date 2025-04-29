8caade1ae2e47ae88492100c63c0557df840c59e
KevinOrellana <kevinorellana202@gmail.com>
2025-04-29 17:47:09 +0200

    change table model Routine_exercise

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -181,7 +181,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\korel\\OneDrive\\Escritorio\\dynamis\\src\\generated\\prisma",
+      "value": "C:\\Users\\ExpertOne2024_1\\Documents\\Kevin\\dynamis-2DAW\\src\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -195,7 +195,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\korel\\OneDrive\\Escritorio\\dynamis\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\ExpertOne2024_1\\Documents\\Kevin\\dynamis-2DAW\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -209,11 +209,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:postgres@localhost:5432/dynamis?schema=public"
+        "value": null
       }
     }
   },
